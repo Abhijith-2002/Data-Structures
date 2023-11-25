f991d463a40f43a22fa9f898f84087ebbe6f0f90
abhijith <xperio135@gmail.com>
2023-11-25 22:17:39 +0530

    Improved readability of code

--- Stack.c
+++ Stack.c
@@ -3,38 +3,40 @@
 int stack[SIZE],top=-1;
 int stackEmpty()
 {
-    if(top==-1)
-    {
-        return 1;
-    }
+    if(top==-1) return 1;
     else return 0;
+}
+int stackFull()
+{
+    if(top==SIZE-1) return 1;
+    else return 0;
+}
+void stackOverflow()
+{
+    printf("Error : Stack Overflow\n");
+}
+void stackUnderflow()
+{
+    printf("Error : Stack Underflow\n");
 }
 void push(int data)
 {
-    if(top==(SIZE-1)) printf("Error : Stack Overflow\n");
-    else
-    {
-        top++;
-        stack[top]=data;
-    }
+    if(stackFull()) stackOverflow();
+    else stack[++top]=data;
 }
 void pop()
 {
-    if(stackEmpty()) printf("Error : Stack Underflow\n");
-    else
-    {
-        top--;
-        printf("Popped : %d\n",stack[top+1]);
-    }
+    if(stackEmpty()) stackUnderflow();
+    else printf("Popped : %d\n",stack[top--]);
 }
 void peek()
 {
-    if(stackEmpty()) printf("Error : Stack is empty\n");
+    if(stackEmpty()) stackUnderflow();
     else printf("Top : %d\n",stack[top]);
 }
 void display()
 {
-    if(stackEmpty()) printf("Error : Stack is empty\n");
+    if(stackEmpty()) stackUnderflow();
     else
     {
         for(int i=0;i<=top;i++)
